6ec83101b8e65b8e274510554f9854e93738ac6b
Abimaelr <abimaelr.albu@gmail.com>
2021-07-27 21:48:26 -0300

    created covidCountry function to fetch the data

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -1,1 +1,9 @@
 //Here are all requests
+
+const covidCountry = async() => {
+    const endPoint = 'https://disease.sh/v3/covid-19/countries';
+    const countryData = fetch(endPoint).then(data => data.json() );
+    return countryData;
+}
+
+export {covidCountry};
